9c0d785edc929eb7154d403c26660a0713d34e54
Caleb Zheng <zheng94@uw.edu>
2026-04-23 13:34:53 -0700

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -18,7 +18,14 @@ conda activate /mmfs1/gscratch/shlneuroai/zheng94/envs/sdib
 # Run your Python script
 # python scripts/inference_pruned_model.py --model flux --num_intervention_steps 5 --dst results/flux_demo_pr20/ --pruned_model_pt ../../hf_cache/ecodiff_flux_prune/schnell/pruned_model_20.pkl 
 # python scripts/evaluation/semantic_eval.py --model flux --pruned_model_pt ../../hf_cache/ecodiff_flux_prune/schnell/pruned_model_20.pkl --task fid
-accelerate launch --num_processes 1 --main_process_port 29501 scripts/evaluation/semantic_eval_dataset.py \
---save_dir results/generated_images_flux_schnell_4steps_pruned_10/ \
---num_intervention_steps 4 --model flux --dataset_name flickr --max_size 100 --image_size 512 \
---pruned_model_pt /mmfs1/gscratch/shlneuroai/zheng94/hf_cache/ecodiff_flux_prune/schnell/pruned_model_20.pkl
+
+
+# accelerate launch --num_processes 1 --main_process_port 29501 scripts/evaluation/semantic_eval_dataset.py \
+# --save_dir results/generated_images_flux_schnell_4steps_pruned_10/ \
+# --num_intervention_steps 4 --model flux --dataset_name flickr --max_size 100 --image_size 512 \
+# --pruned_model_pt /mmfs1/gscratch/shlneuroai/zheng94/hf_cache/ecodiff_flux_prune/schnell/pruned_model_20.pkl
+
+accelerate launch --num_processes 8 scripts/evaluation/semantic_eval_dataset.py \
+--save_dir results/generated_images_flux_dev_28steps_pruned_20/ \
+--num_intervention_steps 28 --model flux_dev --dataset_name flickr --max_size 5000 --image_size 512 \
+--pruned_model_pt /gpfs/projects/shlneuroai/caleb/hf_cache/ecodiff_flux_prune/dev/pruned_model_20.pkl
